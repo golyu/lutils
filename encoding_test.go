7e5c56f2ed38a22b7431610f1b9669648dbd3fdb
lvzhongyi <lvzhongyiforchrome@gmail.com>
2017-07-19 10:22:42 +0800

    加入url.Value数据生成,加入GBK编码转换,

--- encoding_test.go
+++ encoding_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestGBK2UTF8(t *testing.T) {
 	src:=string([]byte{214,208,206,196})
-	log.Println(GBK2UTF8(src))
+	log.Println(GBKDecode_(src))
 }
 
 func TestUTF8_2GBK(t *testing.T) {
 	src:=string([]byte{228,184,173,230,150,135})
 	fmt.Println([]byte(src))
-	log.Println([]byte(UTF8_2GBK(src)))
+	log.Println(GBKEncode_(src))
 }
